14d00eaa908066c9cf83245dccad2d5de289f997
kaban <tikhonov.as@phystech.edu>
2023-11-22 13:11:03 +0300

    clean main

--- src/main.cpp
+++ src/main.cpp
@@ -13,9 +13,6 @@ const char* EVENTLOG_FILENAME = "logs/eventlog";
 const int W = 2000;
 const int H = 1200;
 
-void LoadTxtBtnTextures (Texture textures[4]);
-
-void SetWidgets (Window& mainwin, EventManager& ev_man);
 
 int main() {
     sf::Font fnt;
@@ -92,58 +89,4 @@ int main() {
 
     fclose (eventlogfile);
     return 0;
-}
-
-// void LoadTxtBtnTextures (Texture textures[4]) {
-//     static sf::Texture sftextures[4];
-//     sftextures[0].loadFromFile ("textures/e0.png");
-//     sftextures[1].loadFromFile ("textures/e1.png");
-//     sftextures[2].loadFromFile ("textures/e2.png");
-//     textures[0].sftexture = sftextures + 0;
-//     textures[1].sftexture = sftextures + 1;
-//     textures[2].sftexture = sftextures + 2;
-//     textures[3].sftexture = nullptr;
-// }
-
-void SetWidgets (Window& mainwin, EventManager& ev_man) {
-    static Brush brush (25);
-    static ToolManager tm;
-    tm.setTool (&brush);
-    tm.setColor (Color (255, 0, 128));
-
-    VerticalMenu* tools_vm = new VerticalMenu (405, 105);
-    tools_vm -> AddButton (new ToolBtn (0, 0, 200, 80, "brush", 30, &tm, &brush));
-
-    VerticalMenu* cols_vm = new VerticalMenu (405, 185);
-    cols_vm -> AddButton (new ColorBtn (0, 0, 200, 80, &tm, Color(255, 0, 0)));
-    cols_vm -> AddButton (new ColorBtn (0, 0, 200, 80, &tm, Color(0, 255, 0)));
-    cols_vm -> AddButton (new ColorBtn (0, 0, 200, 80, &tm, Color(0, 0, 255)));
-    cols_vm -> AddButton (new ColorBtn (0, 0, 200, 80, &tm, Color(255, 0, 255)));
-    cols_vm -> AddButton (new ColorBtn (0, 0, 200, 80, &tm, Color(255, 255, 0)));
-    cols_vm -> AddButton (new ColorBtn (0, 0, 200, 80, &tm, Color(0, 255, 255)));
-    cols_vm -> AddButton (new ColorBtn (0, 0, 200, 80, &tm, Color(0, 0, 0)));
-    cols_vm -> AddButton (new ColorBtn (0, 0, 200, 80, &tm, Color(255, 255, 255)));
-
-    static FilterManager fm;
-    static TestFilter test_filter;
-
-    VerticalMenu* filters_vm = new VerticalMenu (405, 265);
-    filters_vm -> AddButton (new FilterBtn (0, 0, 200, 80, "test filter", 30, &fm, &test_filter, &ev_man, &mainwin));
-
-    VerticalMenu* vm = new VerticalMenu (205, 105);
-    vm -> registerSubWidget (tools_vm);
-    vm -> registerSubWidget (cols_vm);
-    vm -> registerSubWidget (filters_vm);
-    vm -> AddButton (new MenuBtn (0, 0, 200, 80,  "tools" , 30,   tools_vm));
-    vm -> AddButton (new MenuBtn (0, 0, 200, 80, "colors" , 30,    cols_vm));
-    vm -> AddButton (new MenuBtn (0, 0, 200, 80, "filters", 30, filters_vm));
-    mainwin.registerSubWidget (vm);
-    mainwin.registerSubWidget (new MenuBtn (205, 25, 200, 80, "edit", 30, vm));
-
-    Window* win = new Window (200, 200, 610, 630);
-    win -> registerSubWidget (new Canvas (5, 25, 600, 600, &tm, &fm));
-    mainwin.registerSubWidget (win);
-    win = new Window (1000, 100, 610, 630);
-    win -> registerSubWidget (new Canvas (5, 25, 600, 600, &tm, &fm));
-    mainwin.registerSubWidget (win);
 }
