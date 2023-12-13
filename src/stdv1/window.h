96c2b62055190e00c3bea1f4a015f29e97e06ee6
kaban <tikhonov.as@phystech.edu>
2023-12-13 08:19:48 +0300

    add window name

--- src/stdv1/window.h
+++ src/stdv1/window.h
@@ -10,11 +10,15 @@ const Color BG_COLOR = Color (192, 192, 192);
 class Window : public Widget {
     Vec2 hold_pos;
     bool is_moving;
+    std::string name;
 
     public:
     bool need_to_close;
 
     explicit Window (double x, double y, double w, double h, bool close_btn = true);
+
+    void SetName(const char* name_) {name.assign(name_);}
+    const char* GetName() {return name.c_str();}
 
     virtual void RenderInRegset (RenderTarget& rt, const RegionSet* to_draw) override;
 
